aa4ed06c3f4037af968cbda0dd64db80b79dbf45
viv231912 <schen231912@gmail.com>
2024-03-19 09:48:11 +1100

    cookies

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
+const cookieParser = require('cookie-parser');
 
 //middleware
 app.use(express.static("public"));
 app.use(express.json());
-
+app.use(cookieParser());
 
 //view engine
 app.set("view engine", "ejs");
 
 //database connection
-const dbURI = "mongodb+srv://schen231912:test1234@cluster0.lmkyipj.mongodb.net/node-auth";
-mongoose.connect(dbURI)
-.then(result => app.listen(3000))
-.catch(error => console.log(error));
+const dbURI =
+  "mongodb+srv://schen231912:test1234@cluster0.lmkyipj.mongodb.net/node-auth";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((error) => console.log(error));
 
 //routes
 //get('/') homepage rendering home view
@@ -25,3 +28,22 @@ app.get("/", (req, res) => {
 });
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+
+//cookies
+//create cookie
+app.get('/set-cookies', (req, res) => {
+  //res.setHeader('Set-Cookie','newUser = true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+
+  res.send('you got the cookie!')
+});
+//read cookie
+app.get('/read-cookies', (req, res) => {
+  
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+});
